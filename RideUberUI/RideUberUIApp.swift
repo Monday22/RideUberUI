0e7de7959e10a763440b197fc41024f9ae3461a4
Anthony K Lartey <mondaynovember22@gmail.com>
2024-12-04 15:53:31 +0000

    Initial Commit

---
+++ RideUberUI/RideUberUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideUberUIApp.swift
+//  RideUberUI
+//
+//  Created by Anthony Lartey on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct RideUberUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
